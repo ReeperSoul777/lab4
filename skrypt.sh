d912e3d2e75333f6e1005ce53a5f8a2736981670
2 <3>
2024-07-04 14:09:16 +0200

    4

--- skrypt.sh
+++ skrypt.sh
@@ -3,17 +3,24 @@ function dzis(){
 echo "Dzisiejsza data: $(date '+%Y-%m-%d')"
 }
 function logi(){
-	for i in $(seq 1 100); do
+echo $1
+  liczba=$1	
+	for i in $(seq 1 $liczba); do
     		file_name="log${i}.txt"
     		echo -e "Nazwa pliku: $file_name\nNazwa skryptu: $(basename "$0")\nData utworzenia: $(date '+%Y-%m-%d')" > "$file_name"
  	done
-
 }
 
 
 
 case "$1" in
 --date) dzis;;
---log) logi;;
+--logs)
+    if [ -z "$2" ]; then
+      logi 3
+    else
+      logi "$2"
+    fi
+    ;;
 
 esac
